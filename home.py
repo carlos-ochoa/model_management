f6065a28972728fbaae3a312ebe8c28efb3c03ed
charlie-ochoa <carlos.ochoa@ab-inbev.com>
2022-10-27 15:36:48 -0500

    a

--- home.py
+++ home.py
@@ -1,6 +1,7 @@
 import pandas as pd
 import streamlit as st
 import requests
+from requests.auth import HTTPBasicAuth
 import os
 
 server = os.environ["MLFLOW_TRACKING_SERVER"]
@@ -9,7 +10,9 @@ st.title("Spotlight Model Management")
 
 st.write(server)
 
-models = requests.get("{server}/api/2.0/preview/mlflow/registered-models/list")
+auth = HTTPBasicAuth(os.environ["MLFLOW_TRACKING_USERNAME"], os.environ["MLFLOW_TRACKING_PASSWORD"])
+
+models = requests.get("{server}/api/2.0/preview/mlflow/registered-models/list", auth = auth)
 
 st.write(models)
 
